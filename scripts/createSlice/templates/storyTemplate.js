e6077e10407ce3d4e8112340d0f70e2245fdc45b
Vadim Niziyov <st1mpro23@yandex.ru>
2023-12-19 20:57:22 +0300

    69 Генератор фичей сущностей страниц на node js

--- scripts/createSlice/templates/storyTemplate.js
+++ scripts/createSlice/templates/storyTemplate.js
@@ -1,6 +1,5 @@
 module.exports = (layer, componentName) => `import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import type { StoryFn, Meta } from '@storybook/react';
 import { ${componentName} } from './${componentName}';
 
 export default {
@@ -9,9 +8,9 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-} as ComponentMeta<typeof ${componentName}>;
+} as Meta<typeof ${componentName}>;
 
-const Template: ComponentStory<typeof ${componentName}> = (args) => <${componentName} {...args} />;
+const Template: StoryFn<typeof ${componentName}> = (args) => <${componentName} {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
