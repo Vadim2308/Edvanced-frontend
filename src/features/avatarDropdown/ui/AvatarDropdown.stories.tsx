3ed6a510fa877813318524cf38fd38761f19b2f7
Vadim Niziyov <st1mpro23@yandex.ru>
2023-12-25 15:35:32 +0300

    79 Popover. NotificationList. RTK query. Polling

---
+++ src/features/avatarDropdown/ui/AvatarDropdown.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+
+import { AvatarDropdown } from './AvatarDropdown';
+
+export default {
+  title: 'features/AvatarDropdown',
+  component: AvatarDropdown,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta<typeof AvatarDropdown>;
+
+const Template: StoryFn<typeof AvatarDropdown> = (args) => (
+  <AvatarDropdown {...args} />
+);
+
+export const Normal = Template.bind({});
+Normal.args = {};
