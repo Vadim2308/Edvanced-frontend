bbe17d2b4fad95bbfb91b9b200d7296e6500effd
Vadim Niziyov <st1mpro23@yandex.ru>
2023-12-27 22:10:09 +0300

    86 StarRating. Модульный подход. Оценка статьи и профиля

---
+++ src/entities/Rating/ui/RatingCard/RatingCard.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+
+import { RatingCard } from './RatingCard';
+
+export default {
+  title: 'entities/RatingCard',
+  component: RatingCard,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta<typeof RatingCard>;
+
+const Template: StoryFn<typeof RatingCard> = (args) => <RatingCard {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
--- src/shared/ui/Popups/components/Popover/Popover.tsx
+++ src/shared/ui/Popups/components/Popover/Popover.tsx
@@ -20,7 +20,9 @@ export function Popover(props: PopoverProps) {
 
   return (
     <HPopover className={classNames(popupCls.popup, {}, [className])}>
-      <HPopover.Button className={popupCls.trigger}>{trigger}</HPopover.Button>
+      <HPopover.Button as='div' className={popupCls.trigger}>
+        {trigger}
+      </HPopover.Button>
 
       <HPopover.Panel className={classNames(cls.panel, {}, menuClasses)}>
         {children}
