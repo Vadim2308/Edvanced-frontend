23fd57affca8ed4d0edecd7a9371f1c3e19a4a97
Vadim Niziyov <st1mpro23@yandex.ru>
2024-01-12 19:43:38 +0300

    minor fix

--- src/pages/ArticleDetailsPage/model/types/ArticleDetailsCommentsSchema.ts
+++ src/pages/ArticleDetailsPage/model/types/ArticleDetailsCommentsSchema.ts
@@ -1,7 +1,8 @@
 import { EntityState } from '@reduxjs/toolkit';
 import { Comment } from '@/entities/Comment';
 
-export interface ArticleDetailsCommentsSchema extends EntityState<Comment> {
+export interface ArticleDetailsCommentsSchema
+  extends EntityState<Comment, Comment['id']> {
   isLoading?: boolean;
   error?: string;
 }
